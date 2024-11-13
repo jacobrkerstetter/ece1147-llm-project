4cb53679b93864b8ed4df667675867fab5070f69
jacob <jacobrkerstetter@gmail.com>
2024-11-13 11:35:31 -0500

    test data added, svm approach tested

--- driver.py
+++
@@ -1,8 +0,0 @@
-from loadData import *
-from PIL import Image
-
-imageLoader = ImageData()
-imageLoader.abortionImages[0].show()
-imageLoader.gunControlImages[0].show()
-
-# getting error loading a lot of images
--- extract_image_features.py
+++ extract_image_features.py
@@ -22,7 +22,7 @@ def load_data(dataset_path, task, bad_tweets_path):
     
     return dataframe, y_true
 
-def handle_IP_approach_1(GC_train_dataset_path, A_train_dataset_path, GC_dev_dataset_path, A_dev_dataset_path, train_dev_images, bad_tweets_path):
+def handle_IP_approach_1(GC_train_dataset_path, A_train_dataset_path, GC_dev_dataset_path, A_dev_dataset_path, GC_test_dataset_path, A_test_dataset_path, train_dev_images, test_images, bad_tweets_path):
 
     GC_df_train, GC_y_train = load_data (GC_train_dataset_path, 'IP', bad_tweets_path)
     A_df_train, A_y_train = load_data (A_train_dataset_path, 'IP', bad_tweets_path)
@@ -34,25 +34,37 @@ def handle_IP_approach_1(GC_train_dataset_path, A_train_dataset_path, GC_dev_dat
     df_dev = pd.concat([A_df_dev, GC_df_dev], axis=0)
     y_dev_true = np.concatenate((A_y_dev, GC_y_dev))
     
+    GC_df_test, GC_y_test = load_data (GC_test_dataset_path, 'IP', bad_tweets_path)
+    A_df_test, A_y_test = load_data (A_test_dataset_path, 'IP', bad_tweets_path)
+    df_test = pd.concat([A_df_test, GC_df_test], axis=0)
+    y_test_true = np.concatenate((A_y_test, GC_y_test))
+
     X_train = multimodal_model.clip32(df_train, train_dev_images)
     X_dev = multimodal_model.clip32(df_dev, train_dev_images)
- 
-    classifier = SVC(kernel='poly', degree=2, C=1.0, coef0=0.02,  shrinking=False, probability=True)
+    X_test = multimodal_model.clip32(df_test, test_images)
+
+    classifier = SVC(kernel='poly', degree=3, C=1.0, coef0=0.02,  shrinking=False, probability=True)
     classifier.fit(X_train, y_train)
  
     y_dev_pred = classifier.predict(X_dev)
+    y_test_pred = classifier.predict(X_test)
     
     pos_label = 'yes'
     dev_f1 = round(f1_score(y_dev_true, y_dev_pred, pos_label=pos_label), 4)
+    test_f1 = round(f1_score(y_test_true, y_test_pred, pos_label=pos_label), 4)
     print ("dev_score: ", dev_f1)
+    print ("test_score: ", test_f1)
 
 if __name__ == "__main__":        
     GC_train_dataset_path = "data/gun_control_train.csv"
     A_train_dataset_path = "data/abortion_train.csv"
     GC_dev_dataset_path = "data/gun_control_dev.csv"
     A_dev_dataset_path = "data/abortion_dev.csv"
+    GC_test_dataset_path = "test/data/gun_control_test.csv"
+    A_test_dataset_path = "test/data/abortion_test.csv"
 
     bad_tweets_path = 'data/bad_tweets.csv'
 
     train_images = "data/images/image/"
-    handle_IP_approach_1(GC_train_dataset_path, A_train_dataset_path, GC_dev_dataset_path, A_dev_dataset_path, train_images, bad_tweets_path)
+    test_images = 'test/data/images/image/'
+    handle_IP_approach_1(GC_train_dataset_path, A_train_dataset_path, GC_dev_dataset_path, A_dev_dataset_path, GC_test_dataset_path, A_test_dataset_path, train_images, test_images, bad_tweets_path)
